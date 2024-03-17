ffe89bf19b59f02882957f9d954ad9a2f56370d3
naim0018 <mdkazinaim0018@gmail.com>
2024-03-18 01:24:44 +0600

    update

--- src/components/shared/Sidebar/Sidebar.js
+++ src/components/shared/Sidebar/Sidebar.js
@@ -1,26 +1,40 @@
 import React from "react";
 import { MdOutlineChevronRight } from "react-icons/md";
+import SidebarHover from "./SidebarHover";
 
 const Sidebar = ({ title, navItems }) => {
+  const options =["price low to high","price high to low","ratings" , "best selling"]
   return (
     <div className="my-20 text-center p-5 space-y-5">
       <p>{title}</p>
-
+      
      
-        <div className="flex items-center justify-between ">
+        <div className="flex items-center justify-between relative group border px-5">
           <p>Sort by Price</p>
           <MdOutlineChevronRight />
+
+
+          <div className="border bg-white absolute z-10 -top-2 -right-36 hidden group-hover:flex  flex-col space-y-3 p-3 ">
+        {
+          options?.map((option,inx)=><SidebarHover key={inx} option={option}/>)
+        }
+        
+      </div>
         </div>
 
-      <div className="space-y-4 ">
+      <div className="space-y-4 px-5 relative group">
         {navItems?.map((nav, inx) => (
           <div key={inx} className="flex items-center justify-between ">
             <p>{nav}</p>
             <MdOutlineChevronRight />
           </div>
         ))}
+    
       </div>
-    </div>
+
+
+        </div>
+  
   );
 };
 
---
+++ src/components/shared/Sidebar/SidebarHover.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarHover = ({option}) => {
+  return (
+    <div>
+      <p className='text-start'>{option}</p>
+    </div>
+  )
+}
+
+export default SidebarHover
